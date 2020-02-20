9846c0aba4a94c2fe1e4b6a5c9fdefc17285e82d
arunthomasalex <arunthomasalex@gmail.com>
2020-02-20 21:23:32 +0530

    application property server

---
+++ application-properties/src/main/java/com/config/server/config/ConfigurationServerApplication.java
@@ -0,0 +1,18 @@
+package com.config.server.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableConfigServer
+@EnableAdminServer
+@SpringBootApplication
+public class ConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServerApplication.class, args);
+	}
+
+}
